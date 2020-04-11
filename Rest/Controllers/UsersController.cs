ccaebb0bc3f939a0537e228379e0f2eb6c7775b3
Garik Hayrapetyan <garhay01@gmail.com>
2020-04-11 23:35:48 +0200

    Add project files.

---
+++ Rest/Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rest.Models;
+
+namespace Rest.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        readonly UsersContext db;
+        public UsersController(UsersContext context)
+        {
+            db = context;
+
+            if (!db.Users.Any()) {
+                db.Users.Add(new User { Name = "Tom", Age = 26 });
+                db.Users.Add(new User { Name = "Alice", Age = 31 });
+                db.SaveChanges();
+            }
+
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await db.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+
+        public string Get(int id)
+        {
+            User user = db.Users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return "Not Found";
+            }
+            return user.Name +" "+user.Id;
+        }
+
+
+     
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+    }
+}
